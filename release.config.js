d2886673007d6433e48dcb0f00ebbe30114d56b2
Thomas Huisman <18439174+Teejeeh@users.noreply.github.com>
2025-08-25 10:35:33 +0200

    bump: add releaseRules

--- release.config.js
+++ release.config.js
@@ -5,6 +5,19 @@ module.exports = {
   branches: ["main"],
   repositoryUrl: "https://github.com/Teejeeh/Angular-IndexedDB-ORM",
   plugins: [
+    ["@semantic-release/commit-analyzer", {
+      preset: "conventionalcommits",
+      releaseRules: [
+        { "type": "feat", "release": "minor" },
+        { "type": "fix", "release": "patch" },
+        { "type": "perf", "release": "patch" },
+        { "type": "refactor", "release": "patch" },
+        { "type": "build", "release": "patch" },
+        { "type": "ci", "release": "patch" },
+        { "type": "chore", "release": "patch" },
+        { "type": "bump", "release": "patch" }
+      ]
+    }],
     ["@semantic-release/release-notes-generator", {
       preset: "conventionalcommits"
     }],
